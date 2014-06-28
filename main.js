711fede1da884383bbbfe5ba3ea42c0309f73ce4
ruslan120101 <ardashev120101@gmail.com>
2014-06-28 01:04:29 -0700

    Begins implementing the ece250 review page.

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-var hidden=0;
+//////////////////////////////////////////////
+//
+// Ruslan Ardashev (ruslan.ardashev@duke.edu)
+// 06/21/14
+//
+//////////////////////////////////////////////
+
+var currPage = "";
+
 
 $(document).ready(function(){
+    
+    $('#loadECE250ReviewPage').click(function() {
+     
+        // Hide home page and header
+        $('#homePage').slideUp(500);
+        $('#header').slideUp(500);
+        
+        // Show back button
+        $('#backButton').slideDown(500);
+        
+        // Show rest of page
+        currPage = "#ece250Page";
+        $('#ece250Page').slideDown(500);
+        
+    });
+    
+    $('#backToHome').click(function() {
+     
+        // Hide current page
+        $(currPage).slideUp(500);
+        
+        // Show header, show home 
+        $('#header').slideDown(500);
+        $('#homePage').slideDown(500);
+        
+    });
     
 });
